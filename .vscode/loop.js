56377c45afe46eccbfa9563a7da26cdb097d349f
asp <asp@aptech.com>
2023-08-01 09:54:20 +0500

    Done

---
+++ .vscode/loop.js
@@ -0,0 +1,16 @@
+let i,num=32
+for( i=1 ;i<=10 ; i++){
+    document.write(num + "X" + i + "=" + i*num + "<br>" )
+} 
+let a=10
+for (a=10 ;a>=1 ; a--)
+document.write (a +  "<br>" )
+
+let r=0;
+do{
+if(r%2!==0){
+    document.write(r+ "<br>")}
+    r++;
+}
+while(r<=15)
+
---
+++ list/app.js
@@ -0,0 +1,6 @@
+console.log('hello');
+document.write('hello');
+let Age=20;
+var FullName='Syed Rayan';
+
+document.write(' welcome ' + FullName + ' your age is ' + Age );
